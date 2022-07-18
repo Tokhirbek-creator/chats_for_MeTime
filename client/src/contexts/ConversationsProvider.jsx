56e7d72a1f085ef8119116e1c12bbfdc6bdc08ae
Tokhir <karamatov.tohirbek@gmail.com>
2022-07-19 00:46:37 +0300

    Sidebar done

--- client/src/contexts/ConversationsProvider.jsx
+++ client/src/contexts/ConversationsProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 import { useContacts } from './ContactsProvider';
 
@@ -10,23 +10,28 @@ export function useConversations() {
 
 export function ConversationsProvider({ children }) {
   const [conversations, setConversations] = useLocalStorage('conversations', []);
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+
   const { contacts } = useContacts();
 
   function createConversation(recipients) {
     setConversations((prevConversations) => [...prevConversations, { recipients, messages: [] }]);
   }
 
-  const formattedConversations = conversations.map((conversation) => {
+  const formattedConversations = conversations.map((conversation, index) => {
     const recipients = conversation.recipients.map((recipient) => {
       const contact = contacts.find((contact) => contact.id === recipient);
       const name = (contact && contact.name) || recipient;
       return { id: recipient, name };
     });
-    return { ...conversation, recipients };
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, recipients, selected };
   });
 
   const value = {
     conversations: formattedConversations,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+    selectConversationIndex: setSelectedConversationIndex,
     createConversation,
   };
 
